1bff2af03ec6f02a5a4fa0cb6ae294476077aaf3
hanseong0308-coder <hanseong0308@gmail.com>
2026-09-16 23:28:43 +0900

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-Python install manager was successfully updated to 26.3.
-
-Additional shebang configuration is now available. Please see
-https://docs.python.org/using/windows#shebang-lines for more information.
-
-********************************************************************************
-The signature for https://www.python.org/ftp/python/index-windows.json was successfully verified.
-Installing Python 3.14.7.
-Downloading: .............
